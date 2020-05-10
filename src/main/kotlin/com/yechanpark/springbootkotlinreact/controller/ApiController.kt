a69178c744f517297aef53345902bd41fe764818
yechanpark <qkrdpcks0419@naver.com>
2020-05-10 13:04:04 +0900

    DI 방식 혼합

--- src/main/kotlin/com/yechanpark/springbootkotlinreact/controller/ApiController.kt
+++ src/main/kotlin/com/yechanpark/springbootkotlinreact/controller/ApiController.kt
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/api")
 class ApiController{
+
+    // Field DI
     @Autowired
     lateinit var boardService: BoardService
 
--- src/main/kotlin/com/yechanpark/springbootkotlinreact/service/BoardService.kt
+++ src/main/kotlin/com/yechanpark/springbootkotlinreact/service/BoardService.kt
@@ -2,13 +2,10 @@ package com.yechanpark.springbootkotlinreact.service
 
 import com.yechanpark.springbootkotlinreact.model.Board
 import com.yechanpark.springbootkotlinreact.repository.BoardRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
-class BoardService {
-    @Autowired
-    lateinit var boardRepository: BoardRepository
+class BoardService(private val boardRepository: BoardRepository) { // constructor DI
 
     fun getBoard(id: Int): Board {
         return boardRepository.getOne(id)
